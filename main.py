cd5234f7088b45323e80144b9541308467b35253
Rowill26 <90589475+Rowill26@users.noreply.github.com>
2026-05-17 10:26:35 +0700

    Create main.py
    Source code buat ngambil data dari file data_wajah.npz

---
+++ main.py
@@ -0,0 +1,10 @@
+import numpy as np
+
+data_muatan = np.load("data_wajah_tersimpan.npz")
+matriks_gamma = data_muatan['matriks']
+label_wajah = data_muatan['label']
+
+print(f"Data siap digunakan!")
+print(f"Ukuran matriks Gamma: {matriks_gamma.shape}")
+
+#lanjutkan codingan lau
